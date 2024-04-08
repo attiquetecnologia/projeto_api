fd17bf6cd3b8e3b3900f5f09b310639a7d9c24d0
Rodrigo Attique <rodrigoatique@gmail.com>
2024-04-08 15:05:33 -0300

    demais funções

--- app.py
+++ app.py
@@ -24,8 +24,16 @@ def arg_implicito():
 def idades():
     from random_data import pessoas
     import funcoes
-    num = funcoes.maior_de_50(pessoas)
-    return jsonify({"status": 200, "data": num})
+    if request.args.get("sexo"):
+        idades = funcoes.maior_2000_sexo(pessoas, sexo="Masculino") # retorna profissões
+        homens = {"total": idades[0], "%":idades[1]}
+        idades = funcoes.maior_2000_sexo(pessoas, sexo="Feminino") # retorna profissões
+        mulheres = {"total": idades[0], "%":idades[1]}
+
+        return jsonify({"status": 200, "data": {"homens": homens, "mulheres":mulheres}})
+    else:
+        num = funcoes.maior_de_50(pessoas)
+        return jsonify({"status": 200, "data": num})
 
 @app.route("/salarios", methods=("GET",))
 def salarios():
@@ -33,3 +41,20 @@ def salarios():
     import funcoes
     num = funcoes.mais_2000(pessoas)
     return jsonify({"status": 200, "data": {"registros": num[2], "porcentagem": num[1], "total": num[0]}})
+
+@app.route("/maiores_salarios", methods=("GET", ))
+def maiores_salarios():
+    from random_data import pessoas
+    import funcoes
+    pessoa1 = funcoes.maior_salario(pessoas)
+    pessoa2 = funcoes.maior_salario(pessoas, float(pessoa1['salario']))
+    pessoa3 = funcoes.maior_salario(pessoas, float(pessoa2['salario']))
+    return jsonify({"status": 200, "data": {"pessoa1": pessoa1
+                                , "pessoa2": pessoa2, "pessoa3": pessoa3}})
+
+@app.route("/profissoes", methods=("GET", ))
+def profissoes():
+    from random_data import pessoas
+    import funcoes
+    profissoes = funcoes.media_profissoes(pessoas)
+    return jsonify({"status": 200, "data": profissoes})
