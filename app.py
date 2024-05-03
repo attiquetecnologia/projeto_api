52f63872c157777466a55ea85e805006b2bace64
Rodrigo Attique <rodrigoatique@gmail.com>
2024-05-03 13:39:10 -0300

    index e main

--- app.py
+++ app.py
@@ -1,6 +1,13 @@
-from flask import Flask, jsonify, request
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
+@app.route("/")
+def index():
+    return render_template("index.html")
+
 from api import bp
 app.register_blueprint(bp)
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0")
