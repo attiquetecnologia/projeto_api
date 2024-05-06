b44578b5959ef8cd4f4fbe75c395cf32b5cd3d0f
Rodrigo Attique <rodrigoatique@gmail.com>
2024-05-06 15:50:50 -0300

    javascript fech funciona

--- static/main.js
+++ static/main.js
@@ -1,50 +1,36 @@
 
 // Events
-document.getElementById("btn_idades").addEventListener("click", function(e){
-    // console.log("Href -> ", this);
-    e.preventDefault();
-    fetch(this.href)
+// select all the buttons with the class name "btn"
+const buttons = document.getElementsByClassName("btn_api");
+
+function exibe_dados(element) {
+    console.log("Href -> ", element);
+    // this.stopPropagation();
+    fetch(element.href)
         .then((resp) => resp.json())
         .then(function(data) {
-            console.log("Data -> ", data.message);
+            console.log("Data -> ", data.data);
     }).catch(function(error) {
         console.log(error);
     });
-});
+};
 
-function createNode(element) {
-    return document.createElement(element);
-}
+// loop through each button and add a click event listener
+for (i=0; i<buttons.length; i++){
+    button = buttons[i];
+    button.addEventListener("click", function(e) {
+        console.log("Href -> ", button);
+        e.stopPropagation();
+        e.preventDefault();
+        fetch(button.href)
+            .then((resp) => resp.json())
+            .then(function(data) {
+                console.log("Data -> ", data.data);
+                return data.map(function(
 
-function append(parent, el) {
-    return parent.appendChild(el);
-}
-
-function index(){
-    console.log("bahh");
-}
-
-function bindex(){
-
-    const ul = document.getElementById('authors');
-    const url = 'https://randomuser.me/api/?results=10';
-
-    fetch(url)
-    .then((resp) => resp.json())
-    .then(function(data) {
-    let authors = data.results;
-    return authors.map(function(author) {
-        let li = createNode('li');
-        let img = createNode('img');
-        let span = createNode('span');
-        img.src = author.picture.medium;
-        span.innerHTML = `${author.name.first} ${author.name.last}`;
-        append(li, img);
-        append(li, span);
-        append(ul, li);
-    })
-    }).catch(function(error) {
-        console.log(error);
+                ));
+        }).catch(function(error) {
+            console.log(error);
+        });
     });
-}//end function
-
+}
